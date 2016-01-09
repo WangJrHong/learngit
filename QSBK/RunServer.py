6b84ba54722b3119a6f3c16668271705787d66a3
WangJrHong <wangzhihong.x@foxmail.com>
2016-01-09 22:10:57 +0800

    2015年改写  初始提交  要提交的变更： 	新文件:       QSBK/.idea/.name 	新文件:       QSBK/.idea/QSBK.iml 	新文件:       QSBK/.idea/encodings.xml 	新文件:       QSBK/.idea/misc.xml 	新文件:       QSBK/.idea/modules.xml 	新文件:       QSBK/.idea/workspace.xml 	新文件:       QSBK/RunServer.py 	新文件:       QSBK/Spider.py 	新文件:       QSBK/__pycache__/Spider.cpython-34.pyc 	新文件:       QSBK/static/css/bootstrap.min.css 	新文件:       QSBK/static/css/font-awesome-min.css 	新文件:       QSBK/static/css/screen.css 	新文件:       QSBK/templates/index.html

---
+++ QSBK/RunServer.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+from Spider import Spider
+app = Flask(__name__)
+spider=Spider()
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html',xiaohualist=spider.xiaohualist)
+
+@app.route('/refresh')
+def home():
+    spider.refresh()
+    return render_template('index.html',xiaohualist=spider.xiaohualist)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ QSBK/Spider.py
@@ -0,0 +1,29 @@
+import urllib.request
+import re
+URL='http://www.qiushibaike.com/text'
+HEADERS={'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+RE=r'<div class="content">(?P<content>.*?)<!--'
+# KA='6221 2868 7001 5182 '
+
+class Spider(object):
+    def __init__(self):
+        '''
+            初始化一个实例,该实例的xiaohualist属性就是我们想要的结果
+        '''
+        self.pattern=re.compile(RE,re.S)
+        page=self._getpage()
+        self._analyzepage(page)
+
+    def refresh(self):
+        page=self._getpage()
+        self._analyzepage(page)
+
+    def _getpage(self):
+        request=urllib.request.Request(URL,headers=HEADERS)
+        return urllib.request.urlopen(request).read().decode()
+
+    def _analyzepage(self,page):
+        self.xiaohualist=[]
+        list=self.pattern.finditer(page)
+        for content in list:
+            self.xiaohualist.append(content.group('content'))
